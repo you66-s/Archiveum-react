4ae9278d7a8e1dd4f13b937763daf493d051e2ba
Youssef <achehbouneyoussef2@gmail.com>
2024-05-10 13:31:29 +0100

    i coded the condition part

--- src/Pages/Add document/Ajouter_Document.js
+++ src/Pages/Add document/Ajouter_Document.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "../Add document/addDoc.css"
 import uploadIcon from "./SVGs/upload-minimalistic-svgrepo-com.svg"
+import checkMarkdown from "./SVGs/checkmark-circle-svgrepo-com.png";
+import cancelMark from "./SVGs/cancel-svgrepo-com.png"
+
+
+
+
+
 function AddDocument() {
     function handleFile() {
         alert("File uploaded")
     }
 
     return (
-        <div className="main-container">
+        <div className="File-container">
             <div className="upload-box">
                 <img className="upload-img" alt="upload" src={uploadIcon} width="125px" height="125px"/>
                 <span id="upload-text">Glisser votre document ici pour le partager</span>
@@ -27,7 +34,7 @@ function AddDocument() {
                         <label style={{display: 'block'}}>Choisir votre filliére</label>
                         <input style={{width: '100%'}} type="text" id="filliere"/>
                     </div>
-                    <div className="module-sec"  style={{width: '50%'}}>
+                    <div className="module-sec" style={{width: '50%'}}>
                         <label style={{display: 'block'}}>Choisir votre filliére</label>
                         <input style={{width: '100%'}} type="text" id="filliere"/>
                     </div>
@@ -49,7 +56,55 @@ function AddDocument() {
                     </div>
                 </div>
                 <div className="Submit-btn" style={{width: '100%'}}>
-                    <button onClick={()=> alert('File uploaded')} id="submit-btn" type="submit">Ajouter Document</button>
+                    <button onClick={() => alert('File uploaded')} id="submit-btn" type="submit">Ajouter Document
+                    </button>
+                </div>
+            </div>
+            <div className="conditions-Sec">
+                <div className="positive-sec">
+                    <div className="content-sec"
+                         style={{display: 'flex', flexDirection: 'row', gap: '10px', alignItems: 'center'}}>
+                        <img id="checkmark" alt="checkMark" src={checkMarkdown} width="24px" height="24px"/>
+                        <p>Partagez des documents, des notes, des resumes et des solutions crees par vous-meme.</p>
+                    </div>
+                    <div className="content-sec"
+                         style={{display: 'flex', flexDirection: 'row', gap: '10px', alignItems: 'center'}}>
+                        <img id="checkmark" alt="checkMark" src={checkMarkdown} width="24px" height="24px"/>
+                        <p>Partagez de preference ces types de fichiers : .pdf, .doc, .docx, .jpeg, .png.</p>
+                    </div>
+                    <div className="content-sec"
+                         style={{display: 'flex', flexDirection: 'row', gap: '10px', alignItems: 'center'}}>
+                        <img id="checkmark" alt="checkMark" src={checkMarkdown} width="24px" height="24px"/>
+                        <p>Veuillez nommer vos documents d'une maniere appropriee et expliquer brievement le contenu
+                            dans la description.</p>
+                    </div>
+                    <div className="content-sec"
+                         style={{display: 'flex', flexDirection: 'row', gap: '10px', alignItems: 'center'}}>
+                        <img id="checkmark" alt="checkMark" src={checkMarkdown} width="24px" height="24px"/>
+                        <p>Assurez-vous que le titre et la description sont informatifs pour vos camarades.</p>
+                    </div>
+                </div>
+                <div className="negative-sec">
+                    <div className="content-sec"
+                         style={{display: 'flex', flexDirection: 'row', gap: '10px', alignItems: 'center'}}>
+                        <img id="checkmark" alt="checkMark" src={cancelMark} width="24px" height="24px"/>
+                        <p>Ne partagez pas des livres entiers.</p>
+                    </div>
+                    <div className="content-sec"
+                         style={{display: 'flex', flexDirection: 'row', gap: '10px', alignItems: 'center'}}>
+                        <img id="checkmark" alt="checkMark" src={cancelMark} width="24px" height="24px"/>
+                        <p>Ne partagez aucun document non lie a vos etudes ou protege par le droit d'auteur.</p>
+                    </div>
+                    <div className="content-sec"
+                         style={{display: 'flex', flexDirection: 'row', gap: '10px', alignItems: 'center'}}>
+                        <img id="checkmark" alt="checkMark" src={cancelMark} width="24px" height="24px"/>
+                        <p>Ne partagez pas de photos individuelles a partir de documents de plusieurs pages.</p>
+                    </div>
+                    <div className="content-sec"
+                         style={{display: 'flex', flexDirection: 'row', gap: '10px', alignItems: 'center'}}>
+                        <img id="checkmark" alt="checkMark" src={cancelMark} width="24px" height="24px"/>
+                        <p>Fusionnez les documents s'ils appartiennent au meme module.</p>
+                    </div>
                 </div>
             </div>
         </div>
